500ee9bec0b9407db632404ca96b0b9a16c45e50
Cristovao Cordeiro <cris@sixsq.com>
2021-09-07 10:40:42 +0200

    fix missing arg

--- code/app.py
+++ code/app.py
@@ -128,7 +128,7 @@ def parse_vulscan_xml(file):
         except AttributeError:
             continue
         if output:
-            output = re.sub('cve.*.csv.*:\n').replace(' |nb| \n\n', '')
+            output = re.sub('cve.*.csv.*:\n', '', output).replace(' |nb| \n\n', '')
             # output = output.replace('cve.csv:\n', '').replace('cve_online.csv:\n', '').replace(' |nb| \n\n', '')
             vulnerabilities_found = output.split(' |nb| ')
             log.info(f"Parsing list of found vulnerabilities for {product}")
